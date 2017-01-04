3cc75f81cb95f188d13a0b8e0b4ee1dc960cec9a
Alice <albano_alice@yahoo.fr>
2017-01-04 15:28:37 +0100

    Re-indented code

--- game_runner.c
+++ game_runner.c
@@ -4,29 +4,29 @@
 
 static bool not_a_winner;
 
-int
-main ()
+int main ()
 {
-  struct Game *a_game = game_new ();
+	struct Game *a_game = game_new ();
 
-  game_add (a_game, "Chet");
-  game_add (a_game, "Pat");
-  game_add (a_game, "Sue");
+	game_add (a_game, "Chet");
+	game_add (a_game, "Pat");
+	game_add (a_game, "Sue");
 
 	srand ((unsigned)time(0));
 
-      do
+	do
 	{
-	  game_roll (a_game, rand () % 5 + 1);
+		game_roll (a_game, rand () % 5 + 1);
 
-	  if (rand () % 9 == 7)
-	    {
-	      not_a_winner = game_wrong_answer (a_game);
-	    }
-	  else
-	    {
-	      not_a_winner = game_was_correctly_answered (a_game);
-	    }
+		if (rand () % 9 == 7)
+		{
+			not_a_winner = game_wrong_answer (a_game);
+		}
+		else
+		{
+			not_a_winner = game_was_correctly_answered (a_game);
+		}
 	}
-      while (not_a_winner);
-  }
+	while (not_a_winner);
+	return EXIT_SUCCESS;
+}
