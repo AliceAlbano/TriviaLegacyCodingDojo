7eb250d48c0b51c64473236cceafb97b440de112
Alice <albano_alice@yahoo.fr>
2017-01-06 13:07:44 +0100

    Correction of a wrong prototype for a getter (too many arguments)

--- question.h
+++ question.h
@@ -4,6 +4,6 @@ struct Question;
 
 struct Question * question_new();
 int get_question_number(struct Question *question);
-enum category get_question_category(struct Question *question, enum category c);
+enum category get_question_category(struct Question *question);
 void set_question_number(struct Question *question, int number);
 void set_question_category(struct Question *question, enum category c);
