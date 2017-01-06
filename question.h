814deea802335780aef0d2f440c01e20a69ae7cf
Alice <albano_alice@yahoo.fr>
2017-01-06 17:47:43 +0100

    Modification of the interface of a question.
    Question now has a setter for its statement, and a getter for its category which is no longer an enum type, but a char*.
    The goal is to use this category to create the statement of the question in the implementation of the setter

--- question.h
+++ question.h
@@ -1,9 +1,9 @@
-enum category{Pop, Science, Sports, Rock};
+#include <string.h>
 
 struct Question;
 
 struct Question * question_new();
 int get_question_number(struct Question *question);
-enum category get_question_category(struct Question *question);
 void set_question_number(struct Question *question, int number);
-void set_question_category(struct Question *question, enum category c);
+void set_question_category(struct Question *question, char c[30], int number);
+void get_question_category(struct Question *question, char c[30]);
