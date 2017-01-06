f0128840642a75f91bb10de2f8eb3c4a9f96a452
Alice <albano_alice@yahoo.fr>
2017-01-06 13:22:23 +0100

    Write a set of tests for getter and setter a question category

--- Tests/question_test.c
+++ Tests/question_test.c
@@ -11,3 +11,16 @@ TEST(question, get_set_question_number)
 	free(question);
 }
 
+TEST(question, get_set_question_category)
+{
+	struct Question *question = question_new();
+	set_question_category(question, Rock);
+	ASSERT_EQ(Rock, get_question_category(question));
+	set_question_category(question, Science);
+	ASSERT_EQ(Science, get_question_category(question));
+	set_question_category(question, Sports);
+	ASSERT_EQ(Sports, get_question_category(question));
+	set_question_category(question, Pop);
+	ASSERT_EQ(Pop, get_question_category(question));
+	free(question);
+}
