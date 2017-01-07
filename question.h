5ae4b6a588ae642adcc0a46ae0a486fb360ffd89
Alice <albano_alice@yahoo.fr>
2017-01-07 11:18:06 +0100

    We write #define for each categories in the interface of a question
    We change the setter for a category, by giving him a cont char * instead of a char *, since categories are constant now

--- question.h
+++ question.h
@@ -1,5 +1,10 @@
 #include <string.h>
 
+#define cat1 "Rock"
+#define cat2 "Pop"
+#define cat3 "Science"
+#define cat4 "Sports"
+
 struct Question;
 
 struct Question * question_new();
@@ -7,4 +12,4 @@ int get_question_number(struct Question *question);
 char * get_question_category(struct Question *question);
 char * get_question_statement(struct Question *question);
 void set_question_number(struct Question *question, int number);
-void set_question_category(struct Question *question, char c[30], int number);
+void set_question_category(struct Question *question, const char c[30], int number);
