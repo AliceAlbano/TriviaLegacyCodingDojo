30bf0685bd26f7d1e26f2fec94cf39bbd26f4330
Alice <albano_alice@yahoo.fr>
2017-01-07 11:00:05 +0100

    Modification of the interface of question : the getter for the category now returns a char *

--- question.h
+++ question.h
@@ -4,6 +4,6 @@ struct Question;
 
 struct Question * question_new();
 int get_question_number(struct Question *question);
+char * get_question_category(struct Question *question);
 void set_question_number(struct Question *question, int number);
 void set_question_category(struct Question *question, char c[30], int number);
-void get_question_category(struct Question *question, char c[30]);
