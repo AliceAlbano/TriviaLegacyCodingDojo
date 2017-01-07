18a5d9bc88c7a1145cbdded55e7e3f5c7a98e499
Alice <albano_alice@yahoo.fr>
2017-01-07 18:10:44 +0100

    [TEST] game: make each test independant from the others

--- Tests/game_test.c
+++ Tests/game_test.c
@@ -2,23 +2,18 @@
 #include "../game.h"
 #include <stdbool.h>
 
-struct Game *game = game_new();
 
-TEST(game_add, first_player )
+TEST(game_add, first_player)
 {
+	struct Game *game = game_new();
 	bool is_added;
+
 	is_added = game_add(game, "First Player");
 	ASSERT_EQ(true, is_added);
 	ASSERT_EQ(get_player_num(game), 1);
 	ASSERT_EQ(get_place(game, 0), 0);
 	ASSERT_EQ(get_purse(game, 0), 0);
 	ASSERT_EQ(get_in_penalty_box(game, 0), false);
+	free(game);
 }
-
-/*
-TEST(firstTest, second_player)
-{
-	ASSERT_EQ(1, 1);
-}
-*/
 
