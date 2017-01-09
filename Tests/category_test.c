614279a4f00862ba72abe5eb47fbc1d3be86b1e1
Alice <albano_alice@yahoo.fr>
2017-01-09 15:49:52 +0100

    First basic test of the getter for the next question

--- Tests/category_test.c
+++ Tests/category_test.c
@@ -8,3 +8,13 @@ TEST(category, get_category_name)
 	ASSERT_EQ(0, strcmp(name, "Rock"));
 	free(category);
 }
+
+TEST(category, get_next_question)
+{
+	struct Question *question = question_new();
+	struct Category *category = category_new("Rock");
+	question = get_next_question(category);
+	ASSERT_EQ(NULL, question);
+	free(category);
+	free(question);
+}
