0106e11e4fdb0a91e916c01c4f37ee3f1795a097
Alice <albano_alice@yahoo.fr>
2017-01-09 16:29:59 +0100

    Add a setter for the next question in a category

--- category.h
+++ category.h
@@ -6,3 +6,4 @@ struct Category;
 struct Category * category_new(const char *name);
 char * get_category_name(struct Category *category);
 struct Question * get_next_question(struct Category *category);
+void set_next_question(struct Category *category);
