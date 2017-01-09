d773d457b45d2ab2e212c902739a00d3f2ef7f0c
Alice <albano_alice@yahoo.fr>
2017-01-09 15:42:05 +0100

    Test of the getter for the category name

--- Tests/category_test.c
+++ Tests/category_test.c
@@ -1,9 +1,10 @@
 #include <gtest/gtest.h>
 #include "../category.h"
 
-TEST(category, first_case)
+TEST(category, get_category_name)
 {
 	struct Category *category = category_new("Rock");
-	ASSERT_EQ(2, 2);
+	char *name = get_category_name(category);
+	ASSERT_EQ(0, strcmp(name, "Rock"));
 	free(category);
 }
