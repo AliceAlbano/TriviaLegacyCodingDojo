88a86c82c98de878cc996fc75d3e3958df28a909
Alice <albano_alice@yahoo.fr>
2017-01-10 18:46:09 +0100

    All the previous test for game were about players, so now this file is almost empty until we write tests about the game

--- Tests/game_test.c
+++ Tests/game_test.c
@@ -6,14 +6,7 @@
 TEST(game_add, first_player)
 {
 	struct Game *game = game_new();
-	bool is_added;
-
-	is_added = game_add(game, "First Player");
-	ASSERT_EQ(true, is_added);
-	ASSERT_EQ(get_player_num(game), 1);
-	ASSERT_EQ(get_place(game, 0), 0);
-	ASSERT_EQ(get_purse(game, 0), 0);
-	ASSERT_EQ(get_in_penalty_box(game, 0), false);
+	ASSERT_EQ(0, 0);
 	free(game);
 }
 
