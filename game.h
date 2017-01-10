0718233b0ba549a319e69846c019ea9dd02ad85f
Alice <albano_alice@yahoo.fr>
2017-01-10 18:44:31 +0100

    Include the player class

--- game.h
+++ game.h
@@ -3,6 +3,7 @@
 
 #include <stdbool.h>
 #include "category.h"
+#include "player.h"
 
 struct Game;
 struct Game *game_new ();
@@ -15,7 +16,7 @@ int get_current_player(struct Game *game);
 int get_available_places(struct Game *game);
 
 bool game_is_playable (struct Game *game);
-bool game_add (struct Game *game, const char *player_name);
+bool game_add (struct Game *game, char *player_name);
 
 void game_roll (struct Game *game, int roll);
 
