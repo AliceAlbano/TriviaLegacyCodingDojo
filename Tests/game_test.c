3d7d37fb500918ae474a1ce2044506c999f104d2
Alice <albano_alice@yahoo.fr>
2017-01-11 18:00:50 +0100

    Tests for game_add function

--- Tests/game_test.c
+++ Tests/game_test.c
@@ -3,10 +3,22 @@
 #include <stdbool.h>
 
 
-TEST(game_add, first_player)
+TEST(game, game_add)
 {
 	struct Game *game = game_new();
-	ASSERT_EQ(0, 0);
+	int i = get_available_places(game);
+	char name[10] = "Alice";
+	bool b;
+	int j;
+	for (j = 0; j < i; j++)
+	{
+		b = game_add(game, name);
+		ASSERT_EQ(b, true);
+		ASSERT_EQ(j + 1, get_player_num(game));
+	}
+	b = game_add(game, name);
+	ASSERT_EQ(b, false);
+	ASSERT_EQ(j, get_player_num(game));
 	free(game);
 }
 
